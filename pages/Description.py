fd13f2361e339d0d130f5bc921986fa46463e978
abhinandhan21 <151395939+abhinandhan21@users.noreply.github.com>
2023-11-28 23:25:57 +0000

    deleted:    pages/Description.py

--- pages/Description.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-# Read the content of README.md
-with open('README.md', 'r') as readme_file:
-    readme_content = readme_file.read()
-
-# Display the README content in the Streamlit app
-st.markdown(readme_content)
